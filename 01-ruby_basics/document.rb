d0692248a37f6f4cb3d2335fcd78a8d9a119a8e9
Cold Head <certainthings@gmail.com>
2013-10-03 12:50:37 +1000

    init

--- 01-ruby_basics/document.rb
+++ 01-ruby_basics/document.rb
@@ -3,7 +3,7 @@ require 'pry'
 class Document
   attr_accessor :title, :author, :content
 
-  def initialize(title, author, content)
+  def initialize(title = nil, author = nil, content = nil)
     puts "Creating new document!"
     @title = title
     @author = author
@@ -23,7 +23,7 @@ Author: #{@author}
   end
 end
 
-doc = Document.new
+doc = Document.new('Some title', 'Some author', 'Some Content')
 doc.title = 'Declaration of Independence'
 doc.author = 'Founding Father Jerks'
 doc.content = 'We hold these truths to be self evident that no one remembers what comes next'
