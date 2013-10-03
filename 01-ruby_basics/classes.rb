48d142bab709f879421e32f801116c3a9867b889
Cold Head <certainthings@gmail.com>
2013-10-03 11:38:25 +1000

    dog example

--- 01-ruby_basics/classes.rb
+++ 01-ruby_basics/classes.rb
@@ -28,7 +28,21 @@ class MarxBrother
   attr_accessor :vice, :instrument, :year_of_birth
 
   def age
-    2013 - @year_of_birth.to_i
+    Time.new.year - @year_of_birth.to_i
+  end
+end
+
+class Dog
+  def year_of_birth
+    @year_of_birth
+  end
+
+  def year_of_birth=(yob)
+    @year_of_birth = yob
+  end
+
+  def age
+    (Time.new.year - @year_of_birth) * 7
   end
 end
 
@@ -38,6 +52,9 @@ harpo = MarxBrother.new
 groucho.vice = 'cigars'
 groucho.instrument = 'guitar'
 groucho.year_of_birth = 1890
+
+fido = Dog.new
+fido.year_of_birth = 1890
 
 binding.pry
 
