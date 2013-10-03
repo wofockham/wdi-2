5dd9ebcda64690d626ae495106d63e8c82de7f43
Cold Head <certainthings@gmail.com>
2013-10-03 10:51:08 +1000

    marx brother class

--- 01-ruby_basics/classes.rb
+++ 01-ruby_basics/classes.rb
@@ -1,5 +1,6 @@
 require 'pry'
 
+=begin
 groucho = {
   :vice => 'cigars',
   :instrument => 'guitar',
@@ -19,9 +20,53 @@ chico = {
 }
 
 def age(brother)
-  2013 - brother[:year_of_birth]
+  2013 - brother[:year_of_birth].to_i
 end
+=end
+
+class MarxBrother
+
+  # vice getter
+  def vice
+    @vice
+  end
+
+  # vice setter
+  def vice=(v)
+    @vice = v
+  end
+
+  # instrument getter
+  def instrument
+    @instrument
+  end
+
+  # instrument setter
+  def instrument=(i)
+    @instrument = i
+  end
+
+  # year of birth getter
+  def year_of_birth
+    @year_of_birth
+  end
+
+  # year of birth getter
+  def year_of_birth=(yob)
+    @year_of_birth = yob
+  end
+
+  def age
+    2013 - @year_of_birth.to_i
+  end
+end
+
+groucho = MarxBrother.new
+harpo = MarxBrother.new
+
+groucho.vice = 'cigars'
+groucho.instrument = 'guitar'
+groucho.year_of_birth = 1890
 
 binding.pry
 
-age(groucho)
