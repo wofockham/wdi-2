1d2d004eed1261011d6d3959fbe6cfec33ecd5db
Cold Head <certainthings@gmail.com>
2013-10-14 13:00:54 +1100

    new product view

--- 04-databases/wdistore/main.rb
+++ 04-databases/wdistore/main.rb
@@ -11,11 +11,20 @@ get '/' do
   erb :index
 end
 
-get '/product/:id' do
+get '/products/new' do
+  # We're just showing a form here so no need to talk to the database.
+  erb :productnew
+end
+
+get '/products/:id' do
   # Gross, we are repeating ourselves.
   connection = PG.connect(:dbname => 'wdistore', :host => 'localhost')
   @product = connection.exec("SELECT * FROM products WHERE id=#{params[:id]}")
   @product = @product.first
+
+  if @product.nil?
+    redirect to '/'
+  end
 
   erb :productview
 end
