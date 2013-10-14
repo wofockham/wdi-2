55c188d2ac90d23007463a65ed7d2aa815037c17
Cold Head <certainthings@gmail.com>
2013-10-14 14:34:02 +1100

    escaping hell

--- 04-databases/wdistore/main.rb
+++ 04-databases/wdistore/main.rb
@@ -16,6 +16,18 @@ get '/products/new' do
   erb :productnew
 end
 
+post '/products/create' do
+  # Welcome to SQL quoting hell.
+
+  # Because the 'quotes' around each of our values has a special meaning, we need to
+  # 'escape' the quotes within our values by replacing ' with \\' using gsub
+  query = "INSERT INTO products (name, description, price) VALUES ('#{params[:name].gsub(/'/, "\\'")}', '#{params[:description].gsub(/'/, "\\'")}', '#{params[:price]}')"
+  connection = PG.connect(:dbname => 'wdistore', :host => 'localhost')
+  binding.pry
+  connection.exec(query)
+  redirect to '/'
+end
+
 get '/products/:id' do
   # Gross, we are repeating ourselves.
   connection = PG.connect(:dbname => 'wdistore', :host => 'localhost')
