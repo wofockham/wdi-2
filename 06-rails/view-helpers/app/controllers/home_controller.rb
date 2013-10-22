600b1a1d57d1daf2cd781e2f9328a8137b3a5026
Cold Head <certainthings@gmail.com>
2013-10-22 11:47:16 +1100

    helpers

--- 06-rails/view-helpers/app/controllers/home_controller.rb
+++ 06-rails/view-helpers/app/controllers/home_controller.rb
@@ -9,11 +9,18 @@ class HomeController < ApplicationController
   end
 
   def text
+    @numbers = (1..20).to_a
+    @person_count1 = 1
+    @person_count2 = 2
+    @story = "A long long long long time ago in a galaxy far far far away"
   end
 
   def assets
   end
 
   def url
+  end
+
+  def test
   end
 end
--- 06-rails/view-helpers/config/routes.rb
+++ 06-rails/view-helpers/config/routes.rb
@@ -1,8 +1,10 @@
 ViewHelpers::Application.routes.draw do
   root :to => 'home#home'
 
-  get '/numbers' => 'home#numbers'
+  get '/numbers2' => 'home#numbers', :as => 'numbers'
   get '/text' => 'home#text'
   get '/assets' => 'home#assets'
   get '/url' => 'home#url'
+
+  post '/test' => 'home#test'
 end
