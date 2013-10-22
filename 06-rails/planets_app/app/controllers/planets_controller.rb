be87d581a8b0ee3f8528afdeca579ab657c16bb1
Cold Head <certainthings@gmail.com>
2013-10-22 14:37:45 +1100

    CRUD

--- 06-rails/planets_app/app/controllers/planets_controller.rb
+++ 06-rails/planets_app/app/controllers/planets_controller.rb
@@ -3,6 +3,31 @@ class PlanetsController < ApplicationController
     @planets = Planet.all
   end
 
+  def show
+    @planet = Planet.find params[:id]
+  end
+
   def new
+  end
+
+  def create
+    Planet.create(params[:planet])
+    redirect_to planets_path
+  end
+
+  def edit
+    @planet = Planet.find(params[:id])
+  end
+
+  def update
+    planet = Planet.find(params[:id])
+    planet.update_attributes(params[:planet])
+    redirect_to planet_path(planet.id)
+  end
+
+  def destroy
+    planet = Planet.find(params[:id])
+    planet.destroy
+    redirect_to planets_path
   end
 end
--- 06-rails/planets_app/config/routes.rb
+++ 06-rails/planets_app/config/routes.rb
@@ -1,6 +1,11 @@
 PlanetsApp::Application.routes.draw do
   root :to => 'planets#index'
-  get '/planets' => 'planets#index'
 
+  get '/planets' => 'planets#index'
+  get '/planets/:id' => 'planets#show', :as => 'planet'
   get '/planets/new' => 'planets#new'
+  post '/planets' => 'planets#create'
+  get '/planets/:id/edit' => 'planets#edit', :as => 'planet_edit'
+  post '/planets/:id' => 'planets#update'
+  post '/planets/:id/delete' => 'planets#destroy', :as => 'planet_delete'
 end
