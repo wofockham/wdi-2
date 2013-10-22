ab52175ce4effa82ed063a801115d8b3dd4d36e6
Cold Head <certainthings@gmail.com>
2013-10-22 12:04:10 +1100

    planet model

---
+++ 06-rails/planets_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PlanetsApp::Application.config.secret_token = '980284a12aafb135b35b2fd3671d0a0a3cc1e7f94ea80ca4d19e5a2b88f0257eeed000b878ae9ff16ab8667573fad90cd0003221e172c655a078e592a7bb2e27'
