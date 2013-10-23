ebf4d313368a9121f6d81166ca260d1159fd2c69
Cold Head <certainthings@gmail.com>
2013-10-23 11:42:07 +1100

    cookbook app and migrations

---
+++ 06-rails/cookbook_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CookbookApp::Application.config.secret_token = '3755bfef8e597f6520c2f9de29009c73aece4af9cb312fbe3f7d0081b852696fb34c676d4b038eb0eb985d76d93b45141c7926234265971757e0c2c9373bc8ed'
