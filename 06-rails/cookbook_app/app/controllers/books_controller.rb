47b163cb6752433407e3a7c220d40d6262adeaa4
Cold Head <certainthings@gmail.com>
2013-10-23 15:19:37 +1100

    checkbox hell

--- 06-rails/cookbook_app/app/controllers/books_controller.rb
+++ 06-rails/cookbook_app/app/controllers/books_controller.rb
@@ -6,4 +6,23 @@ class BooksController < ApplicationController
   def show
     @book = Book.find params[:id]
   end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.create params[:book]
+    redirect_to book_path(book)
+  end
+
+  def edit
+    @book = Book.find params[:id]
+  end
+
+  def destroy
+    book = Book.find params[:id]
+    book.destroy
+    redirect_to books_path
+  end
 end
--- 06-rails/cookbook_app/app/models/book.rb
+++ 06-rails/cookbook_app/app/models/book.rb
@@ -12,6 +12,6 @@
 #
 
 class Book < ActiveRecord::Base
-  attr_accessible :title, :cuisine, :chef, :image
+  attr_accessible :title, :cuisine, :chef, :image, :recipe_ids
   has_many :recipes
 end
