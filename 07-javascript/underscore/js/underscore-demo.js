b44de906639f3fa74695f75d9f7413091bb2ae6b
Cold Head <certainthings@gmail.com>
2013-11-11 14:25:34 +1100

    timers

--- 07-javascript/underscore/js/underscore-demo.js
+++ 07-javascript/underscore/js/underscore-demo.js
@@ -1,5 +1,10 @@
 $(document).ready(function () {
   $('#add_number').click(add_number);
+  $('#square').click(square_numbers);
+  $('#calculate').click(custom_function);
+
+  $('#start').click(start_timer);
+  $('#stop').click(stop_timer);
 });
 
 var numbers = [];
@@ -7,10 +12,38 @@ var numbers = [];
 var add_number = function () {
   var number = parseInt( $('#number').val() );
   numbers.push(number);
-  var $box = $('<div />');
-  $box.addClass('box');
-  $box.text( number );
-  $('#boxes').prepend($box);
+  display_number_box(number);
   $('#number').val('');
   $('#number').focus();
+};
+
+var square_numbers = function () {
+  numbers = _.map(numbers, function (n) { return n * n; });
+  show_numbers();
+};
+
+var custom_function = function () {
+  numbers = _.map(numbers, calculate);
+  show_numbers();
+};
+
+var calculate = function (i) {
+  var formula = $('#formula').val();
+  return eval(formula);
+};
+
+var empty_boxes = function () {
+  $('#boxes').empty();
+};
+
+var show_numbers = function () {
+  empty_boxes();
+  _.each(numbers, display_number_box);
+};
+
+var display_number_box = function (n) {
+  var $box = $('<div />');
+  $box.addClass('box');
+  $box.text( n );
+  $('#boxes').prepend($box);
 };
