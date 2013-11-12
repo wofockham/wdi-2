e6c21ce8b0edb572ac5e8f2edf07c846efe44c39
Cold Head <certainthings@gmail.com>
2013-11-12 13:01:32 +1100

    JS prototype

--- 07-javascript/todo/js/todo.js
+++ 07-javascript/todo/js/todo.js
@@ -1,1 +1,30 @@
+var todo = {
+  tasks: [],
 
+  create_task: function (description) {
+    console.log('creating new task:', description);
+
+    var task = {
+      description: description,
+      creationTime: (new Date()).toString(),
+      completed: false
+    };
+
+    this.tasks.push(task);
+    return task;
+  }
+};
+
+$(document).ready(function () {
+
+  var $description = $('#description');
+
+  $('#create_task').click(function () {
+    var description = $description.val();
+    var task = todo.create_task(description);
+    $description.val('');
+    // Show task via underscore template.
+
+  });
+
+});
