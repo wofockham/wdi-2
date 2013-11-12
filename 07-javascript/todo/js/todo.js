e902c0719a70a998b80905a731acbe5c5ebaa1d8
Cold Head <certainthings@gmail.com>
2013-11-12 15:42:01 +1100

    i have had enough

--- 07-javascript/todo/js/todo.js
+++ 07-javascript/todo/js/todo.js
@@ -1,5 +1,7 @@
 var todo = {
-  tasks: [],
+  tasks: {},
+
+  nextID: 666,
 
   create_task: function (description) {
     console.log('creating new task:', description);
@@ -7,10 +9,17 @@ var todo = {
     var task = {
       description: description,
       creationTime: (new Date()).toString(),
-      completed: false
+      completed: false,
+      id: this.nextID++,
+      /* Let's attach the show() behaviour to the task directly. */
+      show: function () {
+        var template = $('#task_template').html();
+        var task_templater = _.template( template );
+        $('#tasks').append( task_templater( this ) );
+      }
     };
 
-    this.tasks.push(task);
+    this.tasks[task.id] = task;
     return task;
   }
 };
@@ -22,9 +31,42 @@ $(document).ready(function () {
   $('#create_task').click(function () {
     var description = $description.val();
     var task = todo.create_task(description);
+    task.show();
     $description.val('');
-    // Show task via underscore template.
-
   });
 
+  $('#tasks').on('click', '.completed', function () {
+    var $task = $(this).closest('.task');
+    var task_id = $task.data('task-id');
+
+    // Updating the "database".
+    todo.tasks[task_id].completed = true;
+
+    // Updating the page.
+    $('#completed_tasks').append($task);
+    $task.find('.completed').text('Uncomplete');
+    $task.addClass('done');
+  });
+
+  $('#completed_tasks').on('click', '.completed', function () {
+    var $task = $(this).closest('.task');
+    var task_id = $task.data('task-id');
+    todo.tasks[task_id].completed = false;
+    $('#tasks').append($task);
+    $task.removeClass('done');
+    $task.find('.completed').text('Completed');
+  });
+
+  $('#tasks, #completed_tasks').on('click', '.delete', function () {
+    var $task = $(this).closest('.task');
+    var task_id = $task.data('task-id');
+    delete todo.tasks[task_id];
+    $task.fadeOut(function () {
+      $task.remove();
+    });
+  });
+
+  // Seeds.
+  todo.create_task('demo task').show();
+  todo.create_task('further task').show();
 });
