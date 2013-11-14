77ffe6b96cfd09900242260775af02ae9b18266e
Cold Head <certainthings@gmail.com>
2013-11-14 11:43:57 +1100

    login form

--- 07-javascript/todo-rails/config/routes.rb
+++ 07-javascript/todo-rails/config/routes.rb
@@ -1,4 +1,7 @@
 TodoRails::Application.routes.draw do
   root :to => 'pages#index'
   resources :users, :only => [:index, :new, :create]
+  get '/login' => 'session#new'
+  post '/login' => 'session#create'
+  delete '/login' => 'session#destroy'
 end
